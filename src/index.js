d11981cb58de9061d76358ebef2e7183c44378af
reiogo <raogoda2@gmail.com>
2024-07-19 10:22:32 +0900

    Add structure
    Skeleton structure.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Imagon from './flowers.jpg';
+
+console.log('hello');
+
+const element = document.createElement('div')
+
+const myImage = new Image();
+myImage.src = Imagon
+
+  element.appendChild(myImage);
